258072842ade48dd137fec4b13badd677dd467e1
Sungjae Cho <31891751+sungjae-cho@users.noreply.github.com>
2018-06-12 11:36:24 +0900

    Update get_file_list.
    To return the absolute file pathes

--- my-python-util.py
+++ my-python-util.py
@@ -3,5 +3,7 @@ from os.path import isfile, join
 
 
 def get_file_list(dir_path):
-    onlyfiles = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
-    return onlyfiles
+    if dir_path =! '/':
+        dir_path = dir_path + '/'
+    file_list = [ (dir_path + f) for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    return file_list
