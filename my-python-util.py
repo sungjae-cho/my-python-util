043dbc4b2ab9c233ad1b577cbdab945587a205c7
Sungjae Cho <31891751+sungjae-cho@users.noreply.github.com>
2018-06-12 11:37:52 +0900

    Add get_file_name.

--- my-python-util.py
+++ my-python-util.py
@@ -12,3 +12,7 @@ def get_file_list(dir_path):
 def get_extension(file_path):
     _, file_extension = os.path.splitext(file_path)
     return file_extension[1:]
+
+def get_file_name(file_path):
+    file_name, _ = os.path.splitext(file_path)
+    return file_name
