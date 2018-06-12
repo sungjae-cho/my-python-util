8edb1cba9c7d2f9543c668299581dc3e973a00ae
Sungjae Cho <31891751+sungjae-cho@users.noreply.github.com>
2018-06-12 11:37:25 +0900

    Add get_extension.

--- my-python-util.py
+++ my-python-util.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+import os
 
 
 def get_file_list(dir_path):
@@ -7,3 +8,7 @@ def get_file_list(dir_path):
         dir_path = dir_path + '/'
     file_list = [ (dir_path + f) for f in listdir(dir_path) if isfile(join(dir_path, f))]
     return file_list
+
+def get_extension(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension[1:]
