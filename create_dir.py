774839b380b31fef40dd435eae3c499cbfe96b5f
Sungjae Cho <sungjae.cho.1118@gmail.com>
2018-10-19 22:22:35 +0900

    Added discriptions about the create_dir function.

--- create_dir.py
+++ create_dir.py
@@ -1,5 +1,9 @@
 import os
 
 def create_dir(directory):
+    '''
+    - Create a directory if there is no name under `directory`.
+    - Nested directories can be created at once.
+    '''
     if not os.path.exists(directory):
         os.makedirs(directory)
